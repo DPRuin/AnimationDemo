d5cbb8d503d083a857f0b9df25c656c84276284a
mac126 <tulaomao152@126.com>
2018-05-24 18:49:13 +0800

    添加animations和设置多个动画文件切换

--- AnimationDemo/ViewController.swift
+++ AnimationDemo/ViewController.swift
@@ -10,13 +10,41 @@ import UIKit
 import SceneKit
 import ARKit
 
+struct CycleArray<T> {
+    private var array: [T]
+    private var cycleIndex: Int
+    
+    var currentElement: T? {
+        get { return array.count > 0 ? array[cycleIndex] : nil }
+    }
+    
+    init(_ array: [T]) {
+        self.array = array
+        self.cycleIndex = 0
+    }
+    
+    mutating func cycle() -> T?  {
+        cycleIndex = cycleIndex + 1 == array.count ? 0 : cycleIndex + 1
+        return currentElement
+    }
+}
+
+let Animations: [(name: String, file: String)] = [
+    ("Idle",  "idle.dae"),
+    ("Dancing",  "dancing.dae"),
+    ("Jumping", "jumping.dae")]
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
     var heroNode = SCNNode()
+    var animationPlayer: SCNAnimationPlayer!
     var animationLength: TimeInterval!
     var animationFrame: Float!
+    var isZero:Bool = false
+    
+    var heroAnimations = CycleArray(Animations)
     
     @IBOutlet weak var slider: UISlider!
     
@@ -29,29 +57,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        // sceneView.showsStatistics = true
         
         // Create a new scene
-        scene = SCNScene(named: "art.scnassets/dahuangfeng/dahuangfeng2.scn")!
+        scene = SCNScene(named: "art.scnassets/hero.scn")!
         // loadModel()
         
         // Set the scene to the view
         sceneView.scene = scene
         
-        heroNode = scene.rootNode.childNode(withName: "Group001", recursively: true)!
+        heroNode = scene.rootNode.childNode(withName: "hero", recursively: true)!
         
-        let animationPlayer = SCNAnimationPlayer.loadAnimation(fromSceneNamed: "art.scnassets/dahuangfeng/dahuangfeng2.scn")
+        for animation in Animations {
+            let animationPlayer = SCNAnimationPlayer.loadAnimation(fromSceneNamed: "art.scnassets/animations/\(animation.file)")
+            
+            // Adjust animation blend duration for smooth transitions.
+            animationPlayer.animation.blendInDuration = 0.25
+            animationPlayer.animation.blendOutDuration  = 0.5
+            animationPlayer.stop()
+            
+            heroNode.addAnimationPlayer(animationPlayer, forKey: animation.name)
+        }
         
-        // Adjust animation blend duration for smooth transitions.
-        animationPlayer.animation.blendInDuration = 0.25
-        animationPlayer.animation.blendOutDuration  = 0.5
+        heroNode.animationPlayer(forKey: heroAnimations.currentElement!.name)!.play()
         
-        animationPlayer.stop()
-
-        heroNode.addAnimationPlayer(animationPlayer, forKey: "dahuangfeng")
-        animationPlayer.play()
-        // slider.value = 0
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapDidClick(_:)))
+        sceneView.addGestureRecognizer(tap)
         
+        slider.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -76,48 +109,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Release any cached data, images, etc that aren't in use.
     }
     
-    private func loadModel() {
-        scene = SCNScene()
+
+    @objc func tapDidClick(_ gesture: UITapGestureRecognizer) {
+        let currAnimation = heroAnimations.currentElement
+        let nextAnimation = heroAnimations.cycle()
         
-        let myscene = SCNScene(named: "Dahuangfeng", inDirectory: "art.scnassets")!
-        let wrapperNode = SCNNode()
-        for child in myscene.rootNode.childNodes {
-            wrapperNode.addChildNode(child)
-        }
-        heroNode.addChildNode(wrapperNode)
-        heroNode.position = SCNVector3Make(0, 0, 0)
-        heroNode.scale = SCNVector3Make(0.1, 0.1, 0.1)
-        scene.rootNode.addChildNode(heroNode)
+        // Stop current animation.
+        heroNode.animationPlayer(forKey: currAnimation!.name)!.stop(withBlendOutDuration: 1.0)
+        
+        // Play next animation.
+        heroNode.animationPlayer(forKey: nextAnimation!.name)!.play()
+        
     }
-    
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         
-        let animationPlayer = heroNode.animationPlayer(forKey: "dahuangfeng")
-        let animation = animationPlayer?.animation
-        animationLength = (animation?.duration)!
-        
-        print("-1-timeOffset-\(animation?.timeOffset)")
+        let animation = self.animationPlayer?.animation
+        animation?.isCumulative = true
         animation?.timeOffset = Double(sender.value) * animationLength
-        print("-timeOffset-\(animation?.timeOffset)")
-        
         animationPlayer?.speed = 0
     }
-    
-    
 }
 
-// For loading animations with in collada model.
 extension SCNAnimationPlayer {
     class func loadAnimation(fromSceneNamed sceneName: String) -> SCNAnimationPlayer {
         let scene = SCNScene( named: sceneName )!
         // find top level animation
         var animationPlayer: SCNAnimationPlayer! = nil
-//        let dhfnode = scene.rootNode.childNode(withName: "dahuangfeng", recursively: true)!
-//        dhfnode.enumerateChildNodes { (child, stop) in
         scene.rootNode.enumerateChildNodes { (child, stop) in
             if !child.animationKeys.isEmpty {
-                print("--\(child.name)--\(child.animationKeys)")
                 animationPlayer = child.animationPlayer(forKey: child.animationKeys[0])
                 stop.pointee = true
             }
@@ -126,12 +146,3 @@ extension SCNAnimationPlayer {
     }
 }
 
-extension CAAnimation {
-//    class func loadAnimation(fromSceneNamed sceneName: String) -> CAAnimation {
-//        let scene = SCNScene(named: sceneName)
-//        var animation: CAAnimation!
-//        scene?.rootNode.enumerateChildNodes({ (child, stop) in
-//            <#code#>
-//        })
-//    }
-}
